ad912eaf15a03caaee7320267caa105262f754d4
valeriofilareti <valerio.filareti@gmail.com>
2023-07-04 12:49:15 +0200

    add category

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -8,12 +8,20 @@ methods: {
         axios.get('http://127.0.0.1:8000/api/projects')
           .then(result => {
             store.projects = result.data
-            
         })
-      }
+      },
+      // getCategories(){
+      //   axios.get('http://127.0.0.1:8000/api/projects/categories')
+      //     .then(result => {
+      //       store.categories = result.data
+            
+      //   })
+      // },
     },
     mounted() {
-      this.getApi()
+      this.getApi();
+      // this.getCategories();
+      console.log(store.projects);
     },
     data(){
       return {
@@ -25,6 +33,10 @@ methods: {
 
 <template>
 <h1 class="text-center my-5">Projects</h1>
+<!-- <ul>
+  <li v-for="category in store.categories.name" :key="category.id">
+  x</li>
+</ul> -->
 <table class="table table-dark">
   <thead>
     <tr>
@@ -37,7 +49,13 @@ methods: {
   <tbody>
     <tr v-for="(project, index) in store.projects" :key="index">
       <th scope="row">{{project.id}}</th>
-      <td>{{ project.name }}</td>
+      <td>{{ project.name }}
+        <div class="badges">
+          <span class="badge bg-primary m-1">{{ project.category.name }}</span>
+          <span class="badge bg-secondary m-1">tag</span> 
+          <span class="badge bg-secondary m-1">tag</span> 
+        </div>
+      </td>
       <td>{{ project.description }}</td>
       <td><button class="btn btn-primary"><i class="fa-solid fa-eye"></i></button></td>
     </tr>
